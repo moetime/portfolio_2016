72a275121f69e9e0e55ab34474ac93b3cc6d002b
moetime <morrice_pfeiffer@hotmail.com>
2016-10-11 10:06:29 -0400

    Changed home age to reflect new buttons and layout

--- portfolio_2016/routes/index.js
+++ portfolio_2016/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Morrice Portfolio', page: 'Home' });
+  res.render('index', { title: 'Portfolio', page: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', function (req, res, next) {
-    res.render('about', { title: 'About Morrice', page: 'About'  });
+    res.render('about', { title: 'About', page: 'About'  });
     
 });
 
